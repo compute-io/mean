633e3e2b7ed47322247a79e274b9ae1d584fc581
kgryte <kgryte@gmail.com>
2015-01-08 23:37:53 -0800

    [UPDATE] parse CLI args.

--- TODO.md
+++ TODO.md
@@ -3,9 +3,4 @@ TODO
 
 1. cli arguments
 	-	newline, data separator
-	- 	parsing mean stream args
-		-	encoding
-		-	highwatermark
-		-	objectmode
-		-	...
 2. cli tests
--- bin/cli.js
+++ bin/cli.js
@@ -3,13 +3,42 @@
 
 // MODULES //
 
-var byline = require( 'byline' ),
+var parseArgs = require( 'minimist' ),
+	byline = require( 'byline' ),
 	mean = require( './../lib/stream.js' );
+
+
+// ARGUMENTS //
+
+var opts,
+	args;
+
+opts = {
+	'string': [
+		'encoding'
+	],
+	'boolean': [
+		'objectMode',
+		'decodeStrings',
+		'allowHalfOpen'
+	],
+	'default': {
+		'encoding': null, // default: null --> buffer
+		'allowHalfOpen': false, // default: true
+		'highWaterMark': 16, // default: 16kb
+		'objectMode': false, // default: false
+		'decodeStrings': false // default: true
+	}
+};
+
+args = parseArgs( process.argv.slice( 2 ), opts );
 
 
 // STDIN STREAM //
 
+var stream = mean( args );
+
 process.stdin
 	.pipe( new byline.LineStream() )
-	.pipe( mean() );
+	.pipe( stream );
 
