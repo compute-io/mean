68a61a6a2755850f7b81aec571d4d97b9aff483a
kgryte <kgryte@gmail.com>
2015-06-05 00:34:34 -0700

    [TESTS] main interface. matrics and typed arrays.

--- test/test.js
+++ test/test.js
@@ -101,6 +101,15 @@ describe( 'compute-mean', function tests() {
 		assert.strictEqual( mean( data ), expected );
 	});
 
+	it( 'should compute the arithmetic mean of a typed array', function test() {
+		var data, expected;
+
+		data = new Int8Array( [ 2, 4, 5, 3, 8, 2 ] );
+		expected = 4;
+
+		assert.strictEqual( mean( data ), expected );
+	});
+
 	it( 'should compute the arithmetic mean using an accessor function', function test() {
 		var data, expected, actual;
 
@@ -125,6 +134,54 @@ describe( 'compute-mean', function tests() {
 		}
 	});
 
-	it( 'should compute the arithmetic mean along a matrix dimension' );
+	it( 'should compute the arithmetic mean along a matrix dimension', function test() {
+		var expected,
+			data,
+			mat,
+			mu,
+			i;
+
+		data = new Int8Array( 25 );
+		for ( i = 0; i < data.length; i++ ) {
+			data[ i ] = i;
+		}
+		mat = matrix( data, [5,5], 'int8' );
+
+		// Default:
+		mu = mean( mat );
+		expected = '2;7;12;17;22';
+
+		assert.strictEqual( mu.toString(), expected, 'default' );
+
+		// Along columns:
+		mu = mean( mat, {
+			'dim': 2
+		});
+		expected = '2;7;12;17;22';
+
+		assert.strictEqual( mu.toString(), expected, 'dim: 2' );
+
+		// Along rows:
+		mu = mean( mat, {
+			'dim': 1
+		});
+		expected = '10,11,12,13,14';
+
+		assert.strictEqual( mu.toString(), expected, 'dim: 1' );
+	});
+
+	it( 'should compute the arithmetic mean of 1d matrices (vectors)', function test() {
+		var data, mat;
+
+		data = [ 2, 4, 5, 3, 8, 2 ];
+
+		// Row vector:
+		mat = matrix( data, [1,6], 'int8' );
+		assert.strictEqual( mean( mat ), 4 );
+
+		// Column vector:
+		mat = matrix( data, [6,1], 'int8' );
+		assert.strictEqual( mean( mat ), 4 );
+	});
 
 });
